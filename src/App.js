13db5dab391a4663b634350b4c459f0b2ffee4fc
Gad Jacobs <gadexofficial@gmail.com>
2018-10-02 14:44:37 +0100

    update api urls to point to heroku

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://obscure-earth-76272.herokuapp.com:3000/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://obscure-earth-76272.herokuapp.com/   register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -35,7 +35,7 @@ class Register extends Component {
       .then(response => response.json())
       .then(user => {
         if (user.id) {
-          this.props.loadUser(user)
+          this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
       });
